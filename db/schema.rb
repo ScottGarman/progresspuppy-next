9e10cac962ad2d3bce736d88ef04343249169888
Scott Garman <sgarman@zenlinux.com>
2024-12-30 14:54:16 +0700

    Generate Setting model
    Result of running:
    
    bin/rails generate model Setting display_quotes:boolean burnination:boolean user:references:index
    
    Then manually added 'default: true' for display_quotes and 'default:
    false' for burnination.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_28_051055) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_30_075312) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -18,6 +18,15 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_28_051055) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "settings", force: :cascade do |t|
+    t.boolean "display_quotes", default: true
+    t.boolean "burnination", default: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_settings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +42,5 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_28_051055) do
   end
 
   add_foreign_key "sessions", "users"
+  add_foreign_key "settings", "users"
 end
