e70e0efc50f425582d5f7c38d4f30d69ce01c8fe
Scott Garman <sgarman@zenlinux.com>
2024-12-30 15:08:27 +0700

    Generate Quote model
    Result of running:
    
    bin/rails generate model Quote quotation:string source:string user:references:index
    
    Then manually added 'null: false' for quotation and source.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_30_075312) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_30_080743) do
+  create_table "quotes", force: :cascade do |t|
+    t.string "quotation", null: false
+    t.string "source", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_quotes_on_user_id"
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -41,6 +50,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_30_075312) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "quotes", "users"
   add_foreign_key "sessions", "users"
   add_foreign_key "settings", "users"
 end
