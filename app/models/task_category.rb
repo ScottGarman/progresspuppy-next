b17f4dba3c874b0e6aa021385f9ae18b02412a37
Scott Garman <sgarman@zenlinux.com>
2025-01-03 12:40:10 +0700

    Add User unit tests
    This also adds some model validations to other models, which I'll add
    unit tests for in an upcoming commit. Fixtures also needed some fiddling
    to refer to the named user fixtures.

--- app/models/task_category.rb
+++ app/models/task_category.rb
@@ -1,3 +1,11 @@
 class TaskCategory < ApplicationRecord
+  has_many :tasks
   belongs_to :user
+
+  # Validation
+  validates_presence_of :user_id
+
+  validates :name, presence: true,
+                   length: { maximum: 50 },
+                   uniqueness: { scope: :user_id }
 end
