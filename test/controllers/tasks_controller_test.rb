d9773806901ae629ea034ade1bd8fc7436d6457d
Scott Garman <sgarman@zenlinux.com>
2025-01-08 13:08:50 +0700

    Get login/logout process working
    There is no CSS yet, but this allows a user to login and logout. The
    user must already be defined in the db, e.g, done via the rails console.
    This is a first step in bootstrapping some of the original login system
    while making use of the new Rails 8 authentication system.

---
+++ test/controllers/tasks_controller_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class TasksControllerTest < ActionDispatch::IntegrationTest
+  # TODO: Task views require a user to be logged in
+  # test "should get index" do
+  #   get tasks_index_url
+  #   assert_response :success
+  # end
+end
